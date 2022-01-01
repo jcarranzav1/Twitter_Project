d7931dbd0cd07764c2f1aa162c0e25a370e11d56
Juan Diego <jcarranzav1@hotmail.com>
2022-01-01 05:40:53 -0500

    Refactor to create UserProvider

--- projects/react-twitter/src/App.js
+++ projects/react-twitter/src/App.js
@@ -1,21 +1,18 @@
-import React, { useReducer } from 'react';
+import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { Spinner } from 'react-bootstrap';
 
 import Navigation from './components/Navigation';
 import NotFound from './pages/NotFound';
 
-import { AuthContext } from './auth/AuthContext';
-import { authReducer } from './auth/authReducer';
+import { UserProvider } from './auth/AuthContext';
 
 const Home = React.lazy(() => import('./pages/Home'));
 const SignIn = React.lazy(() => import('./pages/auth/SignIn'));
 
 function App() {
-	const [user, setUser] = useReducer(authReducer, {});
-
 	return (
-		<AuthContext.Provider value={{ user, setUser }}>
+		<UserProvider>
 			<Navigation />
 			<Routes>
 				<Route
@@ -38,7 +35,7 @@ function App() {
 				/>
 				<Route path="*" element={<NotFound />} />
 			</Routes>
-		</AuthContext.Provider>
+		</UserProvider>
 	);
 }
 
--- projects/react-twitter/src/auth/AuthContext.js
+++ projects/react-twitter/src/auth/AuthContext.js
@@ -1,2 +1,16 @@
-import { createContext } from 'react';
+import { createContext, useReducer } from 'react';
+import { authReducer } from './authReducer';
+
 export const AuthContext = createContext();
+export function UserProvider({ children }) {
+	const [user, setUser] = useReducer(authReducer, {});
+	return (
+		<AuthContext.Provider
+			value={{
+				user,
+				setUser,
+			}}>
+			{children}
+		</AuthContext.Provider>
+	);
+}
